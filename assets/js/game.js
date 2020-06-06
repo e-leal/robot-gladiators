e3cfa8f7416bd5b6d0695d2d317ab5607cf002fa
e-leal <leal.elyssa03@gmail.com>
2020-06-06 15:06:24 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,32 +57,6 @@ var fight = function(enemyName) {
     }  
 };
 
-
-// function to start a new game
-var startGame = function() {
-    // reset player stats
-    playerHealth = 100;
-    playerAttack = 10;
-    playerMoney = 10;
-    for (var i = 0; i < enemyNames.length; i++) {
-        if (playerHealth > 0) {
-            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    
-            var pickedEnemyName = enemyNames[i];
-    
-            enemyHealth = 50;
-    
-            fight(pickedEnemyName);
-        }
-        else {
-            window.alert("You have lost your robot in battle! Game Over!");
-            break;
-        }
-    }
-    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
-    endGame();
-};
-
   var endGame = function() {
         // if player is still alive, player wins!
         if (playerHealth > 0) {
@@ -102,6 +76,87 @@ var startGame = function() {
             window.alert("Thank you for playing Robot Gladiators! Come back soon!");
         }
     }
+
+
+    var shop = function(){
+        // ask player what they'd like to do
+        var shopOptionPrompt = window.prompt(
+            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        );
+
+        switch(showOptionPrompt) {
+            case "refill":
+                case "REFILL":
+                if(playerMoney >= 7){
+                    window.alert("Refilling player's health by 20 for 7 dollars.");
+                    // increase health and decrease money
+                    playerHealth = playerHealth + 20;
+                    playerMoney = playerMoney - 7;
+                }
+                else {
+                    window.alert("You don't have enough money!");
+                }
+                
+                break;
+            case "upgrade":
+                case "UPGRADE": 
+                if(playerMoney >= 7){
+                    window.alert("Upgrading player's attack by 6 for 7 dollars.");
+                    playerAttack = playerAttack + 6;
+                    playerMoney = playerMoney - 7;
+                }
+                else{
+                    window.alert("You don't have enough money!");
+                }
+                
+                break;
+            case "leave":
+                case "LEAVE":
+                window.alert("Leaving the store.");
+                // do nothing and leave the store
+                break;
+            default: 
+                window.alert("You did not pock a valid option. Try again.");
+                shop();
+                break;
+        }
+    }
+
+
+    // function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0 ) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+            var pickedEnemyName = enemyNames[i];
+    
+            enemyHealth = 50;
+    
+            fight(pickedEnemyName);
+            // if we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                // ask if user wants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                // if yes, take them to the store() function
+                if (storeConfirm) {
+                    shop();
+                }
+            }
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+};
 
 // start the game when the page loads
 startGame();
